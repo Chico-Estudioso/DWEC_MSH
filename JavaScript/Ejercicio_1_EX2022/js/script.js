b3e781053da9554cd9cafa8d3245ce61aaf0b239
Chico-Estudioso <mauroserrano01@educarex.es>
2023-09-27 10:24:34 +0200

    Clase 10

--- JavaScript/Ejercicio_1_EX2022/js/script.js
+++ JavaScript/Ejercicio_1_EX2022/js/script.js
@@ -1,10 +1,14 @@
 window.onload = inicio;
 let btnM = document.getElementById("btn1");
+const componentes = [];
 function inicio() {
+    let contenedorGlobal = document.createElement("div");
+    contenedorGlobal.className = "container";
     let contenedorP = document.createElement("div");
-    contenedorP.className("container");
+    contenedorP.className = "gallery";
     let cuerpo = document.querySelector("body");
-    cuerpo.appendChild(contenedorP);
+    cuerpo.appendChild(contenedorGlobal);
+    contenedorGlobal.appendChild(contenedorP);
     let contenedorTodos = document.createElement("fieldset");
     let contenedorAzul = document.createElement("fieldset");
     let contenedorVerde = document.createElement("fieldset");
@@ -33,6 +37,26 @@ function inicio() {
     contenedorVerde.appendChild(leyendaVerde);
     contenedorRojo.appendChild(leyendaRojo);
     contenedorNaranja.appendChild(leyendaNaranja);
+
+    contenedorTodos.className = "gallery";
+    contenedorAzul.className = "gallery";
+    contenedorVerde.className = "gallery";
+    contenedorRojo.className = "gallery";
+    contenedorNaranja.className = "gallery";
+
+    let colores = ["Orange", "Blue", "Green", "Red"];
+    let coloresT = ["Naranja", "azul", "Verde", "Rojo"];
+
+    for (let i = 0; i < 20; i++) {
+        let num1 = Math.round(Math.random() * 3);
+        let cajaColor = document.createElement("div");
+        cajaColor.className = "gallery div";
+        cajaColor.textContent = coloresT[num1];
+        cajaColor.style.backgroundColor = colores[num1];
+        componentes.push(cajaColor);
+        contenedorTodos.appendChild(cajaColor);
+
+    }
 
 
 }

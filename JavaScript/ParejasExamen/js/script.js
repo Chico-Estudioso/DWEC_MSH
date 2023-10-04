623ecb4d4c780b2d7d968b062e143709515a7605
Chico-Estudioso <mauroserrano01@educarex.es>
2023-10-04 21:12:40 +0200

    Commit

--- JavaScript/ParejasExamen/js/script.js
+++ JavaScript/ParejasExamen/js/script.js
@@ -3,6 +3,8 @@ const ArrayImagenes = ['img/0.JPG', 'img/1.JPG', 'img/2.JPG', 'img/3.JPG', 'img/
 //array con 14 números enteros entre 0 y 7
 const ArrayCajasImg = [];
 const ArrayFinal = [];
+const array1 = [];
+const array2 = [];
 //const pantallaJ = document.querySelector('#pantallaJuego');
 //const resultado = document.querySelector('#resultado');
 function inicio() {
@@ -32,13 +34,38 @@ function inicio() {
     numeros[indice].push(numImagenes);
   }*/
 
-  ArrayCajasImg.for
-
   crearPanel();
 }
 //crear panel juego
 
 function crearPanel() {
+  let numImagenes1;
+  let numImagenes2;
+  for (let i = 0; i < 7; i++) {
+    do {
+      numImagenes1 = Math.round(Math.random() * 7)
+    } while (array1.includes(numImagenes1));
+    array1.push(numImagenes1);
+    let numImagenes1;
+
+    do {
+      numImagenes2 = Math.round(Math.random() * 7)
+    } while (array2.includes(numImagenes2));
+    array2.push(numImagenes2);
+
+    array1.forEach(function (item1, indice1) {
+      ArrayFinal.push(item1);
+
+    });
+    array2.forEach(function (item2, indice2) {
+      ArrayFinal.push(item2);
+    });
+
+    for (let i = ArrayFinal.length - 1; i > 0; i--) {
+      var x = Math.round(Math.random() * (i + 1));
+      [ArrayFinal[i], ArrayFinal[j]] = [ArrayFinal[j], ArrayFinal[i]];
+    }
+  }
 
 }
 

d543fe20d3d70d32211bcdbcba3dcf95e10701a5
Chico-Estudioso <mauroserrano01@educarex.es>
2023-10-11 10:22:06 +0200

    Clase 21

--- JavaScript/RecuperacionPractica/EJ1_12D/ParallaxYosemite/js/script.js
+++ JavaScript/RecuperacionPractica/EJ1_12D/ParallaxYosemite/js/script.js
@@ -3,9 +3,9 @@ window.onload = inicio;
 
 function inicio() {
     let imagen = document.querySelector(".imagen6");
-    imagen.style.background - imagen.url("img/yosemite8.jpg");
-
-    let imagen2 = document.querySelector(".imagen7");
-    imagen2.style.background - imagen.url("img/yosemite9.jpg");
+    imagen.style.cssText = "background-image=url('img/yosemite8.jpg')";
+    imagen.style.backgrounImage = 'url(img/yosemite';
+    /* let imagen2 = document.querySelector(".imagen7");
+     imagen2.style.background - imagen.url("img/yosemite9.jpg");*/
 
 }

c3d9250467949f4f95f1edd08d69c352eba7f7e1
Chico-Estudioso <mauroserrano01@educarex.es>
2023-10-18 08:33:39 +0200

    18/10 Mierc

--- JavaScript/RecuperacionPractica/EJ1_12D/ParallaxYosemite/js/script.js
+++ JavaScript/RecuperacionPractica/EJ1_12D/ParallaxYosemite/js/script.js
@@ -4,8 +4,8 @@ window.onload = inicio;
 function inicio() {
     let imagen = document.querySelector(".imagen6");
     imagen.style.cssText = "background-image=url('img/yosemite8.jpg')";
-    imagen.style.backgrounImage = 'url(img/yosemite';
+    imagen.style.backgrounImage = 'url(img/yosemite'; 
     /* let imagen2 = document.querySelector(".imagen7");
-     imagen2.style.background - imagen.url("img/yosemite9.jpg");*/
-
+    imagen2.style.background - imagen.url("img/yosemite9.jpg");*/
+    
 }

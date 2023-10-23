73f14189ad6424e5a8ffa455b27488ee1733d530
Chico-Estudioso <mauroserrano01@educarex.es>
2023-10-23 12:49:06 +0200

    Ahora si

--- JavaScript/TablaDeUsuarios/js/script.js
+++ JavaScript/TablaDeUsuarios/js/script.js
@@ -1,7 +1,8 @@
-window.onload = inicio;
-let bool = true;
+window.addEventListener("load", inicio);
+
 function inicio() {
-    let formulario = document.getElementById("validar");
+    let formulario = document.getElementById("aceptar");
+
     formulario.onsubmit = function () {
         let pass1 = document.getElementById("password1");
         let smPass1 = document.getElementById("smPassword1");
@@ -9,52 +10,55 @@ function inicio() {
         let pass2 = document.getElementById("password2");
         let smPass2 = document.getElementById("smPassword2");
 
-        let mail1 = document.getElementById("email");
-        let smMail1 = document.getElementById("smEmail1");
-
+        let mail = document.getElementById("email");
+        let smMail = document.getElementById("smEmail");
 
         validarPassword(pass1, smPass1);
-        comprobarPassword(pass1, pass2, smPass2);
-        return bool;
-        function validarPassword(passwd1, smPasswd1) {
-            let cadena = String(passwd1);
-            let longitudCadena = cadena.length;
+        comprobarPassword(pass1, pass2, smPasswd1);
+        bool = true;
+    };
+}
+function validarPassword(passwd1, smPasswd1) {
+    let cadena = String(passwd1);
+    let longitudCadena = cadena.length;
 
-            if (cadena == "") {
-                bool = false;
-                smPasswd1.innerHtml = "*campo obligatorio";
-            } else if (longitudCadena < 8) {
-                bool = false;
-                smPasswd1.innerHtml = "*Debe de tener 8 caracteres";
-            } else {
-                // let mayus = false;
-                // let indice = 0;
-                // while (indice < longitudCadena && !mayus) {
-                //     if (cadena.charAt(indice) == cadena.charAt(indice).toUpperCase) {
-                //         mayus = true;
-                //     }
-                //     indice++;
-                // }
-                // if (mayus == false) {
-                //     bool = false;
-                //     smPasswd1.innerHtml = "*No Hay mayúsculas";
-                // }
-                if (!/[A-Z]/.test(cadena) || !/[a-z]/.test(cadena) || !/[0-9]/.test(cadena)) {
-                    bool = false;
-                    smPasswd1.innerHtml = "La contraseña no cumple los parámetros de seguridad";
-                }
-            }
-        }
-        function comprobarPassword(psswd1, psswd2, smPsswd2) {
-            let cadena = String(pass2);
-            if (psswd1 != psswd2) {
-                bool = false;
-                smPsswd2.innerHtml = "*Las contraseñas no coinciden";
+    if (cadena == "") {
+        bool = false;
+        smPasswd1.innerHTML = "* Campo obligatorio";
+    } else {
+        if (longitudCadena < 8) {
+            bool = false;
+            smPasswd1.innerHTML = "* La contraseña debe tener al menos 8 carácteres";
+        } else {
+            //   let mayus = false;
+            //   let indice = 0;
+            //   while (indice < longitudCadena && !mayus) {
+            //     if (cadena.charAt(indice) == cadena.charAt(indice).toUpperCase) {
+            //       mayus = true;
+            //     }
+            //     indice++;
+            //   }
+            //   if (!mayus) {
+            //     bool = false;
+            //     smPasswd1.innerHTML = "* La contraseña debe tener una mayúscula";
+            //   }
 
+            if (
+                !/[A-Z]/.test(cadena) ||
+                !/[a-z]/.test(cadena) ||
+                !/[0-9]/.test(cadena)
+            ) {
+                bool = false;
+                smPasswd1.innerHTML = "La contraseña no cumple con los requisitos";
             }
         }
     }
+}
+function comprobarPassword(pas1, pas2, smPas2) {
+    let cadena = String(pas2);
 
-
-
+    if (pas1 != pas2) {
+        bool = false;
+        smPas2.innerHTML = "* Las contraseñas no coinciden";
+    }
 }

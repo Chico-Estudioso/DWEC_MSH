6fc30ff8d48d6730afa793cbbd20764b0dd464c4
Chico-Estudioso <mauroserrano01@educarex.es>
2023-11-02 12:01:47 +0100

    fvfgg

--- JavaScript/ParejasExamen/js/script.js
+++ JavaScript/ParejasExamen/js/script.js
@@ -16,14 +16,6 @@ function inicio() {
   contenedorS.className = "grid";
   cuerpo.appendChild(contenedorP);
   contenedorP.appendChild(contenedorS);
-  for (let i = 0; i < 14; i++) {
-    let img = document.createElement("img");
-    img.src = "img/question.png";
-    img.className = "img";
-    contenedorS.appendChild(img);
-    ArrayCajasImg.push(img);
-  }
-
   /*ArrayCajasImg.push(accion);
   let numeros = [];
   function accion(item, indice) {
@@ -33,40 +25,60 @@ function inicio() {
     } while (numeros.includes(numImagenes, numImagenes));
     numeros[indice].push(numImagenes);
   }*/
-
   crearPanel();
 }
 //crear panel juego
-
 function crearPanel() {
-  let numImagenes1;
-  let numImagenes2;
-  for (let i = 0; i < 7; i++) {
-    do {
-      numImagenes1 = Math.round(Math.random() * 7)
-    } while (array1.includes(numImagenes1));
-    array1.push(numImagenes1);
-    let numImagenes1;
+  crearSrcs();
+  let comprobaciones = 0;
+  for (let f = 0; f < 16; f++) {
+    let img = document.createElement("img");
+    img.src = "img/question.png";
+    img.className = ArrayFinal[f];
+    contenedorS.appendChild(img);
+    ArrayCajasImg.push(img);
 
-    do {
-      numImagenes2 = Math.round(Math.random() * 7)
-    } while (array2.includes(numImagenes2));
-    array2.push(numImagenes2);
-
-    array1.forEach(function (item1, indice1) {
-      ArrayFinal.push(item1);
-
-    });
-    array2.forEach(function (item2, indice2) {
-      ArrayFinal.push(item2);
-    });
-
-    for (let i = ArrayFinal.length - 1; i > 0; i--) {
-      var x = Math.round(Math.random() * (i + 1));
-      [ArrayFinal[i], ArrayFinal[j]] = [ArrayFinal[j], ArrayFinal[i]];
+    img.onclick = comprobacion();
+    function comprobacion() {
+      let cajaPulsada;
+      if (comprobacion == 0) {
+        cajaPulsada = img;
+      } else {
+        img.src = "img/ok.png";
+        cajaPulsada.src = "img/ok.png";
+      }
+      comprobaciones = 0;
     }
   }
+  function crearSrcs() {
+    let numImagenes1;
+    let numImagenes2;
+    for (let i = 0; i < 8; i++) {
+      do {
+        numImagenes1 = Math.round(Math.random() * 7)
+      } while (array1.includes(numImagenes1));
+      array1.push(numImagenes1);
+      let numImagenes1;
 
+      do {
+        numImagenes2 = Math.round(Math.random() * 7)
+      } while (array2.includes(numImagenes2));
+      array2.push(numImagenes2);
+      array1.forEach(function (item1, indice1) {
+        ArrayFinal.push(item1);
+      });
+      array2.forEach(function (item2, indice2) {
+        ArrayFinal.push(item2);
+      });
+      for (let i = ArrayFinal.length - 1; i > 0; i--) {
+        var j = Math.round(Math.random() * (i + 1));
+        [ArrayFinal[i], ArrayFinal[j]] = [ArrayFinal[j], ArrayFinal[i]];
+
+
+      }
+    }
+
+  }
 }
 
 

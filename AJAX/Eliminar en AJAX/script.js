b4a1a5f260f4a0c0c608e2e7a43c12244d0a6fd4
Chico-Estudioso <mauroserrano01@educarex.es>
2023-11-08 13:37:58 +0100

    Miércoles 9/11

---
+++ AJAX/Eliminar en AJAX/script.js
@@ -0,0 +1,39 @@
+window.onload = inicio;
+function inicio() {
+    document.getElementById("btnInsertar").addEventListener("click", insertarCiudades);
+}
+function insertarCiudades() {
+    console.log("entro en insertar Ciudades");
+    var id = document.getElementById("_id").value;
+    $.ajax({
+        url: "http://moralo.atwebpages.com/menuAjax/ciudades/EliminarCiudades.php",
+        type: "POST",
+        data: {
+            id: id,
+        }
+    });
+
+
+    mostrar();
+}
+function mostrar() {
+    var cajaMostrarContenido = document.querySelector("#mostrarCiudades");
+    var bloqueHtml = document.createElement("div");
+    var xhr = new XMLHttpRequest;
+    xhr.onreadystatechange = cargar;
+    function cargar() {
+
+        if (this.readyState == 4 && this.status == 200) {
+            var objeto = JSON.parse(this.responseText);
+            objeto.forEach(recorrer);
+            function recorrer(datos, index) {
+                bloqueHtml.innerHTML += "<div class='row'>" +
+                    "<div class='col-lg-2'>" + datos.id + "</div>";
+            }
+            cajaMostrarContenido.appendChild(bloqueHtml);
+        }
+
+    }
+    xhr.open("POST", "http://moralo.atwebpages.com/menuAjax/ciudades/getCiudades.php")
+    xhr.send()
+}
--- AJAX/Insertar en AJAX/script.js
+++ AJAX/Insertar en AJAX/script.js
@@ -1,52 +1,52 @@
-window.onload = inicio;
-
-function inicio() {
-    document.getElementById("btninsertar").addEventListener("click", insertarCiudades);
+window.onload=inicio;
+function inicio(){
+    document.getElementById("btnInsertar").addEventListener("click",insertarCiudades);
 }
-
-function insertarCiudades() {
-    var id = document.querySelector("#_id").value;
-    var nombre = document.querySelector("#_nombre").value;
-    var poblacion = document.querySelector("#_poblacion").value;
-    var densidad = document.querySelector("#_densidad").value;
-    var extension = document.querySelector("#_superficie").value;
+function insertarCiudades(){
+    console.log("entro en insertar Ciudades");
+    var id=document.getElementById("_id").value;
+    var nombre=document.querySelector("#_nombre").value;
+    var poblacion=document.querySelector("#_poblacion").value;
+    var densidad=document.querySelector("#_densidad").value;
+    var extension=document.querySelector("#_superfice").value;
     $.ajax({
-        url: "http://moralo.atwebpages.com/menuAjax/ciudades/insertarCiudades.php",
-        type: "POST",
-        data: {
-            id: id,
-            nombre: nombre,
-            poblacion: poblacion,
-            densidad: densidad,
-            extension: extension
+        url:"http://moralo.atwebpages.com/menuAjax/ciudades/insertarCiudades.php",
+        type:"POST",
+        data:{
+            id:id,
+            nombre:nombre,
+            poblacion:poblacion,
+            densidad:densidad,
+            extension:extension
+
         }
-    })
+    });
 
 
     mostrar();
 }
-function mostrar() {
-    var cajaMostrarContenido = document.querySelector("#mostrar_ciudades");
-    var bloqueHtml = document.createElement("div");
-    var xhr = new XMLHttpRequest;
-    xhr.onreadystatechange = cargar;
-    function cargar() {
-        if (this.readyState == 4 && this.status == 200) {
-            var objeto = JSON.parse(this.responseText);
-            objeto.forEach(recorrer);
-            function recorrer(datos, index) {
-                bloqueHtml.innerHTML += "<div class='row'>" +
-                    "<div class='col-lg-2'>" + datos.id + "</div>" +
-                    "<div class='col-lg-2'>" + datos.nombre + "</div>" +
-                    "<div class='col-lg-2'>" + datos.poblacion + "</div>" +
-                    "<div class='col-lg-2'>" + datos.densidad + "</div>" +
-                    "<div class='col-lg-2'>" + datos.extension + "</div>" +
-                    "</div>";
-                cajaMostrarContenido.appendChild(bloqueHtml);
+function mostrar(){
+    var cajaMostrarContenido=document.querySelector("#mostrarCiudades");
+    var bloqueHtml=document.createElement("div");
+    var xhr=new XMLHttpRequest;
+    xhr.onreadystatechange=cargar;
+    function cargar(){
 
-            }
+        if (this.readyState==4 && this.status==200){
+          var objeto=JSON.parse(this.responseText);
+          objeto.forEach(recorrer);
+          function recorrer(datos, index){
+             bloqueHtml.innerHTML+="<div class='row'>"+
+             "<div class='col-lg-2'>"+datos.id+"</div>"+
+             "<div class='col-lg-2'>"+datos.nombre+"</div>"+
+             "<div class='col-lg-2'>"+datos.poblacion+"</div>"+
+             "<div class='col-lg-2'>"+datos.densidad+"</div>"+
+             "<div class='col-lg-2'>"+datos.extension+"</div></div>";
+          }
+          cajaMostrarContenido.appendChild(bloqueHtml);
         }
-        xhr.open("POST", "http://code.jquery.com/menuAjax/ciudades/insertarCiudades.php");
-        xhr.send();
+       
     }
+    xhr.open("POST","http://moralo.atwebpages.com/menuAjax/ciudades/getCiudades.php")
+    xhr.send()
 }

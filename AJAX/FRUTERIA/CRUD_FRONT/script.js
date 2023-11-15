bdd1edb2fd2e728e815242fd4be89c08a6a64cff
Chico-Estudioso <mauroserrano01@educarex.es>
2023-11-15 10:21:44 +0100

    Miércoles 15/11

--- AJAX/FRUTERIA/CRUD_FRONT/script.js
+++ AJAX/FRUTERIA/CRUD_FRONT/script.js
@@ -1,39 +1,54 @@
-window.onload=inicio;
-let cajaFrutas=document.querySelector("#cajaFrutas");
-cajaFrutas.innerHTML="";
-let bloqueHtml=document.createElement("div");
+window.onload = inicio;
+let cajaFrutas = document.querySelector("#cajaFrutas");
+cajaFrutas.innerHTML = "";
+let bloqueHtml = document.createElement("div");
 var objeto;
+let bloqueCesta = document.createElement("div");
+let contenedorCesta = document.querySelector("tbody");
 
-function inicio(){
+function inicio() {
     cargarContenido();
     console.log(objeto);
 }
-function cargarFrutas(){
-    bloqueHtml.className="row";
-    for(let i=0;i<objeto.length;i++){
-        let vector=[];
+function cargarFrutas() {
+    bloqueHtml.className = "row";
+    for (let i = 0; i < objeto.length; i++) {
+        let vector = [];
         vector.push(objeto[i].id, objeto[i].name, objeto[i].photo, objeto[i].price);
-        bloqueHtml.innerHTML+=
-        '<div class="col-lg-4">'+
-        '<img class="card-img-top" src='+objeto[i].photo+' width="90" height="90" alt='+objeto[i].id+'>'+
-        '<div class="card-body" onclick=anadirCesta("'+vector+'")>'+
-        '<h4 class="card-title">'+objeto[i].name+'</h4>'+
-        '<p class="card-text">'+objeto[i].price+'</p></div></div>';
+        bloqueHtml.innerHTML +=
+            '<div class="col-lg-4">' +
+            '<img class="card-img-top" src=' + objeto[i].photo + ' width="90" height="90" alt=' + objeto[i].id + '>' +
+            '<div class="card-body" onclick=anadirCesta("' + vector + '")>' +
+            '<h4 class="card-title">' + objeto[i].name + '</h4>' +
+            '<p class="card-text">' + objeto[i].price + '</p></div></div>';
     }
     cajaFrutas.appendChild(bloqueHtml);
 }
-function cargarContenido(){
-    var xhr=new XMLHttpRequest;
-    xhr.onreadystatechange=function(){
-        if(this.readyState==4 && this.status==200){
-            objeto=JSON.parse(this.responseText);
+function cargarContenido() {
+    var xhr = new XMLHttpRequest;
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            objeto = JSON.parse(this.responseText);
             cargarFrutas();
         }
     }
-    xhr.open("GET","http://moralo.atwebpages.com/menuAjax/productos/index.php", true);
+    xhr.open("GET", "http://moralo.atwebpages.com/menuAjax/productos/index.php", true);
     xhr.send();
 }
-function anadirCesta(vector){
+function anadirCesta(vector) {
     console.log(vector);
+    // let cajaTr = document.createElement("tr");
+    // let cajaTd = document.createElement("td");
+    let vectorX = vector.split(",");
+    let peso = prompt("Teclea los Kg de: " + vectorX[1]);
+    let calculoPrecio = peso * parseFloat(vectorX[3]);
 
+    if (peso && !isNaN(peso)) {
+        bloqueCesta.innerHTML += '<tr><td>' +
+            vectorX[1] + '</td>' + "<td>" + peso + "</td>" + "<td>" + vectorX[3] + "</td>" + "<td>" + calculoPrecio + "</td></tr>";
+
+    }
+    // cajaTr.appendChild(cajaTd);
+    // bloqueCesta.appendChild(cajaTr);
+    contenedorCesta.appendChild(bloqueCesta);
 }

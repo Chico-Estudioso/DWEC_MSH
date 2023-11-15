afd9ee091d1cfdf40151b70b899917b5424986d8
Chico-Estudioso <mauroserrano01@educarex.es>
2023-11-15 10:00:18 +0100

    Miércoles 15/11 Desesperación

--- AJAX/FRUTERIA/CRUD_FRONT/script.js
+++ AJAX/FRUTERIA/CRUD_FRONT/script.js
@@ -1,44 +1,39 @@
-window.onload = inicio;
-
-let cajaFrutas = document.querySelector("#cajaFrutas");
-cajaFrutas.innerHTML = "";
-let bloqueHtml = document.createElement("div");
+window.onload=inicio;
+let cajaFrutas=document.querySelector("#cajaFrutas");
+cajaFrutas.innerHTML="";
+let bloqueHtml=document.createElement("div");
 var objeto;
-function inicio() {
+
+function inicio(){
     cargarContenido();
-}
-
-function cargarFrutas() {
-    console.log("1");
-
     console.log(objeto);
-    bloqueHtml.className = "row";
-    for (let i = 0; i < objeto.length; i++) {
-        bloqueHtml.innerHTML +=
-            "<div class='col-lg-4'>" +
-            "<img class='card-img-top' src='" + objeto[i].photo + "' width='90px' height='90px' alt='" + objeto[i].id + "' />" +
-            "<div class='card - body'>" +
-            "<h4 class='card - title'>" + objeto[i].name + "</h4>" +
-            "<p class='card - text'>" + objeto[i].price + "</p>" +
-            "</div>" +
-            "</div>";
+}
+function cargarFrutas(){
+    bloqueHtml.className="row";
+    for(let i=0;i<objeto.length;i++){
+        let vector=[];
+        vector.push(objeto[i].id, objeto[i].name, objeto[i].photo, objeto[i].price);
+        bloqueHtml.innerHTML+=
+        '<div class="col-lg-4">'+
+        '<img class="card-img-top" src='+objeto[i].photo+' width="90" height="90" alt='+objeto[i].id+'>'+
+        '<div class="card-body" onclick=anadirCesta("'+vector+'")>'+
+        '<h4 class="card-title">'+objeto[i].name+'</h4>'+
+        '<p class="card-text">'+objeto[i].price+'</p></div></div>';
     }
     cajaFrutas.appendChild(bloqueHtml);
-
 }
-
-function cargarContenido() {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            objeto = JSON.parse(this.responseText);
+function cargarContenido(){
+    var xhr=new XMLHttpRequest;
+    xhr.onreadystatechange=function(){
+        if(this.readyState==4 && this.status==200){
+            objeto=JSON.parse(this.responseText);
             cargarFrutas();
         }
-        xhr.open(
-            "GET",
-            " http://moralo.atwebpages.com/menuAjax/productos/index.php",
-            true
-        );
-        xhr.send();
     }
+    xhr.open("GET","http://moralo.atwebpages.com/menuAjax/productos/index.php", true);
+    xhr.send();
+}
+function anadirCesta(vector){
+    console.log(vector);
+
 }

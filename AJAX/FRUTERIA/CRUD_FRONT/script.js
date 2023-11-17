90bc4badb0c7facd9ab9a159e07de1386e382707
Chico-Estudioso <mauroserrano01@educarex.es>
2023-11-17 09:13:57 +0100

    Viernes 17/11

--- AJAX/FRUTERIA/CRUD_FRONT/script.js
+++ AJAX/FRUTERIA/CRUD_FRONT/script.js
@@ -2,9 +2,12 @@ window.onload = inicio;
 let cajaFrutas = document.querySelector("#cajaFrutas");
 cajaFrutas.innerHTML = "";
 let bloqueHtml = document.createElement("div");
-var objeto;
 let bloqueCesta = document.createElement("div");
-let contenedorCesta = document.querySelector("tbody");
+var objeto;
+let contenedorCesta = document.querySelector("#cestaCompra");
+var total = 0;
+var calculoPrecio = 0;
+let precioTotal = document.getElementById("precio");
 
 function inicio() {
     cargarContenido();
@@ -17,38 +20,81 @@ function cargarFrutas() {
         vector.push(objeto[i].id, objeto[i].name, objeto[i].photo, objeto[i].price);
         bloqueHtml.innerHTML +=
             '<div class="col-lg-4">' +
-            '<img class="card-img-top" src=' + objeto[i].photo + ' width="90" height="90" alt=' + objeto[i].id + '>' +
-            '<div class="card-body" onclick=anadirCesta("' + vector + '")>' +
-            '<h4 class="card-title">' + objeto[i].name + '</h4>' +
-            '<p class="card-text">' + objeto[i].price + '</p></div></div>';
+            '<img class="card-img-top" onclick=anadirCesta("' +
+            vector +
+            '") src=' +
+            objeto[i].photo +
+            ' width="90" height="90" alt=' +
+            objeto[i].id +
+            ">" +
+            '<div class="card-body" onclick=anadirCesta("' +
+            vector +
+            '")>' +
+            '<h4 class="card-title">' +
+            objeto[i].name +
+            "</h4>" +
+            '<p class="card-text">' +
+            objeto[i].price +
+            "</p></div></div>";
     }
+
     cajaFrutas.appendChild(bloqueHtml);
 }
 function cargarContenido() {
-    var xhr = new XMLHttpRequest;
+    var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             objeto = JSON.parse(this.responseText);
             cargarFrutas();
         }
-    }
-    xhr.open("GET", "http://moralo.atwebpages.com/menuAjax/productos/index.php", true);
+    };
+    xhr.open(
+        "GET",
+        "http://moralo.atwebpages.com/menuAjax/productos/index.php",
+        true
+    );
     xhr.send();
 }
 function anadirCesta(vector) {
+    let cajaTr = document.createElement("tr");
     console.log(vector);
-    // let cajaTr = document.createElement("tr");
-    // let cajaTd = document.createElement("td");
     let vectorX = vector.split(",");
-    let peso = prompt("Teclea los Kg de: " + vectorX[1]);
-    let calculoPrecio = peso * parseFloat(vectorX[3]);
+    let peso = prompt("Teclea los kgs de " + vectorX[1]);
+    calculoPrecio = peso * parseFloat(vectorX[3]);
+
+    total = total + calculoPrecio;
+    precioTotal.textContent = total;
 
     if (peso && !isNaN(peso)) {
-        bloqueCesta.innerHTML += '<tr><td>' +
-            vectorX[1] + '</td>' + "<td>" + peso + "</td>" + "<td>" + vectorX[3] + "</td>" + "<td>" + calculoPrecio + "</td></tr>";
-
+        cajaTr.innerHTML =
+            "<td>" +
+            vectorX[1] +
+            "</td><td>" +
+            peso +
+            "</td><td>" +
+            vectorX[3] +
+            "</td><td>" +
+            calculoPrecio +
+            "</td>" +
+            "<td>" + //simular botón con a href añado clase btn btn-danger (color rojo)
+            "<div class='col-lg-2 text-center mb-2'><a class='btn btn-danger btn-md'" +
+            //anulo el href, no hay link , pero sí hay evento onclick con
+            //parámetro incluido: dni de esa tupla
+            " href='javascript:void(0)' onclick=eliminar(this,'" +
+            calculoPrecio +
+            "')>" +
+            //texto del botón e icono
+            "ELIMINAR<i class='bi-trash'></i></a></div> " +
+            "</td>";
     }
-    // cajaTr.appendChild(cajaTd);
-    // bloqueCesta.appendChild(cajaTr);
-    contenedorCesta.appendChild(bloqueCesta);
+    contenedorCesta.appendChild(cajaTr);
+}
+
+function eliminar(fila, calculo) {
+    let filaTabla = fila.parentNode.parentNode;
+    // console.log("CL 1:Objeto-->" + fila.parentNode.parentNode);
+    filaTabla.parentNode.remove(filaTabla);
+    total = total - calculo;
+    precioTotal.textContent = total;
+
 }
